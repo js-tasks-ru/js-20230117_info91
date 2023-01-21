9345dd8ee870a41d10a77322813390bd6479202d
Дмитрий <info@consultapp.ru>
2023-01-21 10:52:53 +0400

    fns sortString works

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -4,6 +4,14 @@
  * @param {string} [param="asc"] param - the sorting type "asc" or "desc"
  * @returns {string[]}
  */
-export function sortStrings(arr, param = 'asc') {
+export function sortStrings(arr, param = "asc") {
+  const result = [...arr];
 
+  result.sort(function (a, b) {
+    return a.localeCompare(b.toUpperCase());
+  });
+
+  if (param === "desc") result.reverse();
+
+  return result;
 }
