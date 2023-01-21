bf5e75461853eb322ed91bf75b8cb659abf40df5
javascriptru <67475495+jsru-1@users.noreply.github.com>
2023-01-21 06:31:01 +0300

    Added task sortStrings

---
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -0,0 +1,9 @@
+/**
+ * sortStrings - sorts array of string by two criteria "asc" or "desc"
+ * @param {string[]} arr - the array of strings
+ * @param {string} [param="asc"] param - the sorting type "asc" or "desc"
+ * @returns {string[]}
+ */
+export function sortStrings(arr, param = 'asc') {
+
+}
