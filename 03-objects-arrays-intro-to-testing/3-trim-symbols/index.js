a9138d0b6e19e72e67db4cdecc877066f7e32ecf
Дмитрий <info@consultapp.ru>
2023-01-25 01:00:16 +0400

    fns trimSymbols works

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,20 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
-
+  return typeof size != "undefined"
+    ? string.split("").reduce(
+        (s, char) => {
+          if (s.curChar !== char && size > 0) {
+            s.counter = 1;
+            s.curChar = char;
+            s.str += char;
+          } else if (s.counter < size) {
+            s.counter++;
+            s.str += char;
+          }
+          return s;
+        },
+        { str: "", counter: 0, curChar: "" }
+      ).str
+    : string;
 }
