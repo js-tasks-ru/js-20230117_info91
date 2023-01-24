266ab710fcc10b57c1bd36f74ef6181f289c3fb9
javascriptru <67475495+jsru-1@users.noreply.github.com>
2023-01-24 22:46:11 +0300

    Added task invertObj

---
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -0,0 +1,8 @@
+/**
+ * invertObj - should swap object keys and values
+ * @param {object} obj - the initial object
+ * @returns {object | undefined} - returns new object or undefined if nothing did't pass
+ */
+export function invertObj(obj) {
+
+}
