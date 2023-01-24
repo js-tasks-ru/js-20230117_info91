486b99f1f179e91956577afba712dad59c0d03a3
Дмитрий <info@consultapp.ru>
2023-01-24 23:44:14 +0400

    fns createGetter works

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -3,6 +3,13 @@
  * @param {string} path - the strings path separated by dot
  * @returns {function} - function-getter which allow get value from object by set path
  */
-export function createGetter(path) {
 
+export function createGetter(field) {
+  return (obj) => {
+    return field.split(".").reduce((current, item) => {
+      return current && Object.hasOwn(current, item)
+        ? current[item]
+        : undefined;
+    }, obj);
+  };
 }
