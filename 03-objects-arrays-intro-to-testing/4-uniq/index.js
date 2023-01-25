fcc05764fd48eb5adde91079f26e5da4299786db
Дмитрий <info@consultapp.ru>
2023-01-25 14:46:08 +0400

    fns uniq works

--- 03-objects-arrays-intro-to-testing/4-uniq/index.js
+++ 03-objects-arrays-intro-to-testing/4-uniq/index.js
@@ -4,5 +4,16 @@
  * @returns {*[]} - the new array with uniq values
  */
 export function uniq(arr) {
+  if (typeof arr === "undefined" || arr.length === 0) return [];
 
+  // Object.keys(
+  return arr.reduce(
+    (itt, char) =>
+      typeof itt.obj[char] === "undefined" &&
+      (itt.obj[char] = 1) &&
+      itt.arr.push(char)
+        ? itt
+        : itt,
+    { obj: {}, arr: [] }
+  ).arr;
 }
