1684aad20a13a83139ca8b26352bea5cc9b3500e
javascriptru <67475495+jsru-1@users.noreply.github.com>
2023-01-25 13:12:27 +0300

    Added task uniq

---
+++ 03-objects-arrays-intro-to-testing/4-uniq/index.js
@@ -0,0 +1,8 @@
+/**
+ * uniq - returns array of uniq values:
+ * @param {*[]} arr - the array of primitive values
+ * @returns {*[]} - the new array with uniq values
+ */
+export function uniq(arr) {
+
+}
