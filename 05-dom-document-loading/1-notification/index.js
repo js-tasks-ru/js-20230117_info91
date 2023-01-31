5ae3018c7ca5dfaebe7b414159b9e44fb266979f
Дмитрий <info@consultapp.ru>
2023-01-31 23:53:01 +0400

    start class

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,39 @@
 export default class NotificationMessage {
+  static executed = false;
+  _element = null;
 
+  constructor(msg = "", { duration = 2000, type = "success" } = {}) {
+    this.render();
+  }
+
+  get element() {
+    return this._element;
+  }
+  set element(e) {
+    this._element = e;
+  }
+
+  render() {
+    this.element = document.createElement("div");
+    this.element.innerHTML = `
+      <div class="notification success" style="--value:20s">
+        <div class="timer"></div>
+        <div class="inner-wrapper">
+          <div class="notification-header">success</div>
+          <div class="notification-body">Hello world</div>
+        </div>
+      </div>
+    `;
+
+    this.element = this.element.firstElementChild;
+    console.log(this.element);
+  }
+
+  show() {}
+
+  remove() {}
+
+  destroy() {}
 }
+
+// git pull upstream master
