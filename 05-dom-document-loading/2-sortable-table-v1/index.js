8f244a22fdd34696e813b32412fba0b58849a675
Дмитрий <info@consultapp.ru>
2023-02-02 19:42:15 +0400

    lesson5 class SortableTable works

--- 05-dom-document-loading/2-sortable-table-v1/index.js
+++ 05-dom-document-loading/2-sortable-table-v1/index.js
@@ -3,6 +3,7 @@ export default class SortableTable {
 
   constructor(headerConfig = [], data = []) {
     this._headerConfig = headerConfig;
+    this._currentSort = { fieldValue: "", orderValue: "" };
     this._allowSorting = new Map(
       headerConfig
         .filter((v) => {
@@ -13,6 +14,7 @@ export default class SortableTable {
         })
     );
     this._data = data;
+
     this._render();
   }
 
@@ -20,8 +22,8 @@ export default class SortableTable {
     const wrap = document.createElement("div");
     wrap.innerHTML = this._getTemplate();
     this._element = wrap.firstElementChild;
-    if (!this._body)
-      this._body = this._element.querySelector(".sortable-table__body");
+    const tmpQ = this._element.querySelectorAll("div[data-element]");
+    this._subElements = { head: tmpQ[0], body: tmpQ[1] };
   }
 
   _getTemplate() {
@@ -35,21 +37,21 @@ export default class SortableTable {
     `;
   }
 
-  // id: 'title',
-  // title: 'Name',
-  // sortable: true,
-  // sortType: 'string'
-
   _getHeaderTemplate() {
     return this.isConfigNotEmpty()
       ? `<div
         data-element="header"
         class="sortable-table__header sortable-table__row"
       >
-        ${this._headerConfig
-          .map((item) => this._getHeaderCellTemplate(item))
-          .join("")}
+        ${this._getHeaderInnerTemplate()}
       </div>`
+      : "";
+  }
+  _getHeaderInnerTemplate() {
+    return this.isConfigNotEmpty()
+      ? this._headerConfig
+          .map((item) => this._getHeaderCellTemplate(item))
+          .join("")
       : "";
   }
   _getHeaderCellTemplate(cell) {
@@ -57,11 +59,11 @@ export default class SortableTable {
     class="sortable-table__cell"
     data-id="${cell.id}"
     data-sortable="${cell.sortable}"
-    data-order="asc"
+    data-order="${this._currentSort.orderValue}"
   >
     <span>${cell.title}</span>
-
-  </div>`; //${this._getSortArrowTemplate(cell.id)}
+    ${this._getSortArrowTemplate(cell.id)}
+  </div>`;
   }
 
   _getBodyTemplate() {
@@ -92,9 +94,9 @@ export default class SortableTable {
         <img
           class="sortable-table-image"
           alt="${cell[0].source ?? ""}"
-          src=""
+          src="${cell[0].url ?? ""}"
         />
-      </div>`; /*${cell[0].url ?? ""}*/
+      </div>`;
     return `<div class="sortable-table__cell">${cell ?? ""}</div>`;
   }
   _getLoadingTemplate() {
@@ -108,25 +110,32 @@ export default class SortableTable {
     </div>
     </div>`;
   }
-  _getSortArrowTemplate() {
-    return `
-      <span data-element="arrow" class="sortable-table__sort-arrow"><span class="sort-arrow"></span></span>`;
+  _getSortArrowTemplate(id) {
+    return this._currentSort.orderValue && id === this._currentSort.fieldValue
+      ? `<span data-element="arrow" class="sortable-table__sort-arrow"><span class="sort-arrow"></span></span>`
+      : "";
   }
 
   sort(fieldValue, orderValue) {
-    if (this._allowSorting.has(fieldValue)) {
-      switch (this._allowSorting.get(fieldValue)) {
-        case "string":
-          this._sortStrings(fieldValue, orderValue);
-          break;
-        default:
-          this._data.sort((a, b) => {
-            return orderValue === "asc"
-              ? a[fieldValue] - b[fieldValue]
-              : b[fieldValue] - a[fieldValue];
-          });
+    if (
+      this._currentSort.fieldValue !== fieldValue ||
+      this._currentSort.orderValue !== orderValue
+    ) {
+      this._currentSort = { fieldValue: fieldValue, orderValue: orderValue };
+      if (this._allowSorting.has(fieldValue)) {
+        switch (this._allowSorting.get(fieldValue)) {
+          case "string":
+            this._sortStrings(fieldValue, orderValue);
+            break;
+          default:
+            this._data.sort((a, b) => {
+              return orderValue === "asc"
+                ? a[fieldValue] - b[fieldValue]
+                : b[fieldValue] - a[fieldValue];
+            });
+        }
+        this.updateData();
       }
-      this.updateData();
     }
   }
   _sortStrings(fieldName, param = "asc") {
@@ -143,7 +152,8 @@ export default class SortableTable {
       this._render();
     }
     if (this.isDataLoaded()) {
-      this._body.innerHTML = this._getBodyInnerTemplate();
+      this.subElements.body.innerHTML = this._getBodyInnerTemplate();
+      this.subElements.head.innerHTML = this._getHeaderInnerTemplate();
     }
   }
 
@@ -158,6 +168,9 @@ export default class SortableTable {
 
   get element() {
     return this._element;
+  }
+  get subElements() {
+    return this._subElements;
   }
   isConfigNotEmpty() {
     return this._headerConfig && this._headerConfig.length > 0;
