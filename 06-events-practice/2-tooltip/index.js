b5217e23850219d0cc45894fb5ab11dee5e44ab7
Дмитрий <info@consultapp.ru>
2023-02-04 19:35:15 +0400

    lesson6 class Tooltip works

--- 06-events-practice/2-tooltip/index.js
+++ 06-events-practice/2-tooltip/index.js
@@ -1,50 +1,53 @@
 class Tooltip {
-  element = null;
-  MOUSE_INDENT = 5; // отступ в пикселях от указателя мыши
+  static element = null;
+  static MOUSE_INDENT = 5; // отступ в пикселях от указателя мыши, добавляет плавность и видимость текста
 
-  move = (event) => {
-    this.element.style.left = event.clientX + this.MOUSE_INDENT + "px";
-    this.element.style.top = event.clientY + this.MOUSE_INDENT + "px";
+  constructor() {
+    return Tooltip;
+  }
+
+  static render(text, parent = document.body) {
+    Tooltip.element.textContent = text;
+    parent.append(Tooltip.element);
+  }
+
+  static initialize() {
+    Tooltip.element = document.createElement("div");
+    Tooltip.element.className = "tooltip";
+    document.addEventListener("pointerover", Tooltip.over);
+    document.addEventListener("pointerout", Tooltip.out);
+  }
+
+  static move = (event) => {
+    Tooltip.element.style.left = `${event.clientX + Tooltip.MOUSE_INDENT}px`;
+    Tooltip.element.style.top = `${event.clientY + Tooltip.MOUSE_INDENT}px`;
   };
 
-  over = (event) => {
-    const parent = event.target;
-    if (parent.dataset.tooltip && this.element) {
-      this.element.textContent = parent.dataset.tooltip;
-      parent.append(this.element);
-
-      parent.addEventListener("mousemove", this.move);
-    }
-  };
-  out = (event) => {
+  static over = (event) => {
     const parent = event.target;
     if (parent.dataset.tooltip) {
-      parent.removeEventListener("mousemove", this.move);
-      this.remove();
+      Tooltip.render(parent.dataset.tooltip, parent);
+      parent.addEventListener("mousemove", Tooltip.move);
+    }
+  };
+  static out = (event) => {
+    const parent = event.target;
+    if (parent.dataset.tooltip) {
+      parent.removeEventListener("mousemove", Tooltip.move);
+      Tooltip.remove();
     }
   };
 
-  render() {
-    this.element = document.createElement("div");
-    this.element.className = "tooltip";
-  }
-
-  initialize() {
-    this.render();
-    document.addEventListener("mouseover", this.over);
-    document.addEventListener("mouseout", this.out);
-  }
-
-  remove() {
-    if (this.element) {
-      this.element.remove();
+  static remove() {
+    if (Tooltip.element) {
+      Tooltip.element.remove();
     }
   }
-  destroy() {
-    this.remove();
-    document.removeEventListener("mouseover", this.over);
-    document.removeEventListener("mouseout", this.out);
-    this.element = null;
+  static destroy() {
+    Tooltip.remove();
+    document.removeEventListener("pointerover", Tooltip.over);
+    document.removeEventListener("pointerout", Tooltip.out);
+    Tooltip.element = null;
   }
 }
 
