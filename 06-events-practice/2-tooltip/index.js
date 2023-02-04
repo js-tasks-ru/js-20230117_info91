6ff87d2f9323212099d537bb5f75a5670eb136d1
Дмитрий <info@consultapp.ru>
2023-02-04 18:39:50 +0400

    tooltip start

--- 06-events-practice/2-tooltip/index.js
+++ 06-events-practice/2-tooltip/index.js
@@ -1,6 +1,50 @@
 class Tooltip {
-  initialize () {
+  element = null;
+  MOUSE_INDENT = 5; // отступ в пикселях от указателя мыши
 
+  move = (event) => {
+    this.element.style.left = event.clientX + this.MOUSE_INDENT + "px";
+    this.element.style.top = event.clientY + this.MOUSE_INDENT + "px";
+  };
+
+  over = (event) => {
+    const parent = event.target;
+    if (parent.dataset.tooltip && this.element) {
+      this.element.textContent = parent.dataset.tooltip;
+      parent.append(this.element);
+
+      parent.addEventListener("mousemove", this.move);
+    }
+  };
+  out = (event) => {
+    const parent = event.target;
+    if (parent.dataset.tooltip) {
+      parent.removeEventListener("mousemove", this.move);
+      this.remove();
+    }
+  };
+
+  render() {
+    this.element = document.createElement("div");
+    this.element.className = "tooltip";
+  }
+
+  initialize() {
+    this.render();
+    document.addEventListener("mouseover", this.over);
+    document.addEventListener("mouseout", this.out);
+  }
+
+  remove() {
+    if (this.element) {
+      this.element.remove();
+    }
+  }
+  destroy() {
+    this.remove();
+    document.removeEventListener("mouseover", this.over);
+    document.removeEventListener("mouseout", this.out);
+    this.element = null;
   }
 }
 
