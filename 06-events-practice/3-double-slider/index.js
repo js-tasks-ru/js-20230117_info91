ff25df8266bc55698b351378ab5c6db4a52ad3d5
Дмитрий <info@consultapp.ru>
2023-02-06 15:16:05 +0400

    base functions works

--- 06-events-practice/3-double-slider/index.js
+++ 06-events-practice/3-double-slider/index.js
@@ -7,19 +7,18 @@ export default class DoubleSlider {
   static MIN_PERCENT = 0;
 
   constructor({
-    minVal = 0,
-    maxVal = 100,
+    minVal = 1,
+    maxVal = 101,
     step = 1,
     left = 10,
     right = 90,
-
     currencyTemptate = (data) => `$${data}`,
   } = {}) {
     this._left = left;
     this._right = right;
     this.minVal = minVal;
     this.maxVal = maxVal;
-    this.step = step;
+    this.step = step > 0 ? step : 0;
     this.currencyTemptate = currencyTemptate;
 
     this.render();
@@ -46,11 +45,51 @@ export default class DoubleSlider {
         this.subElements[i++] = item;
       }
     }
-    console.log(this.subElements);
+  }
+
+  move = (event) => {
+    if (this.clickTarget === this.subElements.right) {
+      this.right = this.pixelToPersent(event.clientX - this.baseCoords.x);
+    }
+    if (this.clickTarget === this.subElements.left) {
+      this.left = this.pixelToPersent(event.clientX - this.baseCoords.x);
+    }
+
+    // console.log(
+    //   "move",
+    //   event.clientX,
+    //   this.baseCoords.x,
+    //   event.clientX - this.baseCoords.x,
+    //   this.pixelToPersent(event.clientX - this.baseCoords.x),
+    //   this.left,
+    //   this.right
+    // );
+
+    this.update();
+  };
+
+  pixelToPersent(pixel) {
+    return Number((pixel / this.mousePersentStep).toFixed(this.step));
+  }
+
+  pointerDown = (event) => {
+    this.baseCoords = this.subElements.inner.getBoundingClientRect();
+    this.mousePersentStep = this.baseCoords.width / 100;
+    this.clickTarget = event.target.closest("span");
+
+    document.addEventListener("mousemove", this.move);
+  };
+
+  init() {
+    this.subElements.left.addEventListener("pointerdown", this.pointerDown);
+    this.subElements.right.addEventListener("pointerdown", this.pointerDown);
+    document.addEventListener("pointerup", () => {
+      this.clickTarget = {};
+      document.removeEventListener("mousemove", this.move);
+    });
   }
 
   update() {
-    console.log("update", this.left, this.right, this.subElements.right);
     this.updateValues();
 
     this.subElements.left.style.left = this.left + "%";
@@ -65,56 +104,20 @@ export default class DoubleSlider {
     this.values.forEach((val, index) => {
       this.subElements[index].textContent = this.currencyTemptate(val);
     });
-    // this.subElements.
-  }
-
-  move = (event) => {
-    console.log(
-      event.clientX,
-      this.pixelToPersent(event.clientX - this.baseCoords.x)
-    );
-
-    if (this.clickTarget === this.subElements.right) {
-      this.right = this.pixelToPersent(event.clientX - this.baseCoords.x);
-    }
-    if (this.clickTarget === this.subElements.left) {
-      this.left = this.pixelToPersent(event.clientX - this.baseCoords.x);
-    }
-    this.update();
-  };
-
-  pixelToPersent(pixel) {
-    return (pixel / this.mousePersentStep).toFixed(this.step);
-  }
-
-  pointerDown = (event) => {
-    this.baseCoords = this.subElements.inner.getBoundingClientRect();
-    this.mousePersentStep = this.baseCoords.width / 100;
-
-    console.log(this.mousePersentStep, this.baseCoords.width, this.baseCoords);
-    this.clickTarget = event.target.closest("span");
-
-    document.addEventListener("mousemove", this.move);
-
-    document.addEventListener("pointerup", () => {
-      this.clickObj = {};
-      document.removeEventListener("mousemove", this.move);
-    });
-  };
-
-  init() {
-    this.subElements.left.addEventListener("pointerdown", this.pointerDown);
-    this.subElements.right.addEventListener("pointerdown", this.pointerDown);
   }
 
   updateValues() {
     this.values = [this.getFromValue(), this.getToValue()];
   }
   getFromValue() {
-    return (this.minVal + (this.range * this.left) / 100).toFixed(this.step);
+    return parseFloat(
+      (this.minVal + (this.range * this.left) / 100).toFixed(this.step)
+    );
   }
   getToValue() {
-    return (this.minVal + (this.range * this.right) / 100).toFixed(this.step);
+    return parseFloat(
+      (this.minVal + (this.range * this.right) / 100).toFixed(this.step)
+    );
   }
 
   get left() {
@@ -124,12 +127,20 @@ export default class DoubleSlider {
     return this._right;
   }
   set left(val) {
-    if (this._right > val && val >= DoubleSlider.MIN_PERCENT) {
+    if (val < DoubleSlider.MIN_PERCENT) {
+      this._left = DoubleSlider.MIN_PERCENT;
+    } else if (val >= this.right) {
+      this._left = this.right - 10 ** -this.step;
+    } else {
       this._left = val;
     }
   }
   set right(val) {
-    if (this._left < val && val <= DoubleSlider.MAX_PERCENT) {
+    if (val > DoubleSlider.MAX_PERCENT) {
+      this._right = DoubleSlider.MAX_PERCENT;
+    } else if (val <= this.left) {
+      this._right = this.left + 10 ** -this.step;
+    } else {
       this._right = val;
     }
   }
@@ -145,7 +156,6 @@ export default class DoubleSlider {
     this.element = null;
   }
   getTemplate() {
-    console.dir(this);
     return `  <div class="range-slider">
     <span>${this.currencyTemptate(this.values[0])}</span>
     <div class="range-slider__inner">
