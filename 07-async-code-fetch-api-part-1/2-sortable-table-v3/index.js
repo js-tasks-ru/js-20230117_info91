6cb470272308132f54730024bafcbb4d2659416b
javascriptru <67475495+jsru-1@users.noreply.github.com>
2023-02-09 17:16:10 +0300

    Added task SortableTable v3 с динамической загрузкой данных и "infinity scroll"

---
+++ 07-async-code-fetch-api-part-1/2-sortable-table-v3/index.js
@@ -0,0 +1,20 @@
+import fetchJson from './utils/fetch-json.js';
+
+const BACKEND_URL = 'https://course-js.javascript.ru';
+
+export default class SortableTable {
+  constructor(headersConfig, {
+    data = [],
+    sorted = {}
+  } = {}) {
+
+  }
+
+  sortOnClient (id, order) {
+
+  }
+
+  sortOnServer (id, order) {
+
+  }
+}
