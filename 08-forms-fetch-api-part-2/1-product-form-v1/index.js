34c140caf580458bc64a6d7ab9d425d850d73fd6
Дмитрий <info@consultapp.ru>
2023-02-12 16:43:07 +0400

    ProductForm all tests without last

--- 08-forms-fetch-api-part-2/1-product-form-v1/index.js
+++ 08-forms-fetch-api-part-2/1-product-form-v1/index.js
@@ -6,38 +6,95 @@ const BACKEND_URL = "https://course-js.javascript.ru";
 
 export default class ProductForm {
   element = null;
+  controller = new AbortController();
+  product = null;
+  categories = null;
+  subElements = {};
+  CAT_API_URL = "api/rest/categories";
+  PRODUCT_API_URL = "api/rest/products";
   constructor(productId) {
     this.productId = productId;
   }
 
   async render() {
     const wrap = document.createElement("div");
-    wrap.innerHTML = getTemplate();
+
+    await this.loadData();
+
+    wrap.innerHTML = this.getTemplate();
     this.element = wrap.firstElementChild;
+    this.getSubElements();
+
+    return this.element;
   }
 
+  getSubElements() {
+    for (const item of this.element.querySelectorAll("[data-element]")) {
+      this.subElements[item.dataset.element] = item;
+    }
+  }
+
+  async loadData() {
+    const promises = [];
+    promises.push(this.loadCategories());
+    if (this.productId) {
+      promises.push(this.loadProduct());
+    }
+
+    try {
+      const data = await Promise.all(promises);
+      if (data[0] && data[0].length > 0) {
+        this.categories = data[0];
+      }
+      if (data[1] && data[1][0]) {
+        this.product = data[1][0];
+      }
+      // console.dir(this.categories);
+      // console.dir(this.product);
+    } catch (e) {
+      console.error("Error of data loading. " + e);
+    }
+  }
+
+  loadCategories() {
+    const query = new URL(this.CAT_API_URL, BACKEND_URL);
+    query.searchParams.set("_sort", "weight");
+    query.searchParams.set("_refs", "subcategory");
+
+    return fetchJson(query);
+  }
+  loadProduct() {
+    if (this.productId) {
+      const query = new URL(this.PRODUCT_API_URL, BACKEND_URL);
+      query.searchParams.set("id", this.productId);
+
+      return fetchJson(query);
+    }
+  }
+
+  //  TEMPLATES
   getTemplate() {
     return `<div class="product-form">
         <form data-element="productForm" class="form-grid">
-        ${this.getTitleTemplate}
-        ${this.getDescriptionTemplate}
-        ${this.getPhotoTemplate}
-        ${this.getCategoriesTemplate}
-        ${this.getPropertiesTemplate}
-        ${this.getPhotoTemplate}
-        ${this.getButtonsTemplate}
+        ${this.getTitleTemplate()}
+        ${this.getDescriptionTemplate()}
+        ${this.getPhotosTemplate()}
+        ${this.getCategoriesTemplate()}
+        ${this.getPropertiesTemplate()}
+        ${this.getButtonsTemplate()}
         </form>
       </div>`;
   }
-
   getTitleTemplate() {
     return `<div class="form-group form-group__half_left">
         <fieldset>
           <label class="form-label">Название товара</label>
           <input
+            value="${this.product ? escapeHtml(this.product.title) : ""}"
             required=""
             type="text"
             name="title"
+            id="title"
             class="form-control"
             placeholder="Название товара"
           />
@@ -52,12 +109,13 @@ export default class ProductForm {
           required=""
           class="form-control"
           name="description"
+          id="description"
           data-element="productDescription"
           placeholder="Описание товара"
-        ></textarea>
+        >${this.product ? escapeHtml(this.product.description) : ""}</textarea>
       </div>`;
   }
-  getPhotoTemplate() {
+  getPhotosTemplate() {
     return `<div
         class="form-group form-group__wide"
         data-element="sortable-list-container"
@@ -65,33 +123,15 @@ export default class ProductForm {
         <label class="form-label">Фото</label>
         <div data-element="imageListContainer">
           <ul class="sortable-list">
-            <li
-              class="products-edit__imagelist-item sortable-list__item"
-              style=""
-            >
-              <input
-                type="hidden"
-                name="url"
-                value="https://i.imgur.com/MWorX2R.jpg"
-              />
-              <input
-                type="hidden"
-                name="source"
-                value="75462242_3746019958756848_838491213769211904_n.jpg"
-              />
-              <span>
-                <img src="icon-grab.svg" data-grab-handle="" alt="grab" />
-                <img
-                  class="sortable-table__cell-img"
-                  alt="Image"
-                  src="https://i.imgur.com/MWorX2R.jpg"
-                />
-                <span>75462242_3746019958756848_838491213769211904_n.jpg</span>
-              </span>
-              <button type="button">
-                <img src="icon-trash.svg" data-delete-handle="" alt="delete" />
-              </button>
-            </li>
+            ${
+              this.product
+                ? this.product.images
+                    .map((photo) => {
+                      return this.getPhotoTemplate(photo);
+                    })
+                    .join("")
+                : ""
+            }
           </ul>
         </div>
         <button type="button" name="uploadImage" class="button-primary-outline">
@@ -99,27 +139,64 @@ export default class ProductForm {
         </button>
       </div>`;
   }
+  getPhotoTemplate(photo) {
+    return `<li class="products-edit__imagelist-item sortable-list__item" style="">
+        <input
+          type="hidden"
+          name="url"
+          value="${photo.url || ""}"
+        />
+        <input
+          type="hidden"
+          name="source"
+          value="${photo.source || ""}"
+        />
+        <span>
+          <img src="icon-grab.svg" data-grab-handle="" alt="grab" />
+          <img
+            class="sortable-table__cell-img"
+            alt="Image"
+            src="${photo.url || ""}"
+          />
+          <span>${photo.source || ""}</span>
+        </span>
+        <button type="button">
+          <img src="icon-trash.svg" data-delete-handle="" alt="delete" />
+        </button>
+      </li>`;
+  }
   getCategoriesTemplate() {
     return `<div class="form-group form-group__half_left">
       <label class="form-label">Категория</label>
-      <select class="form-control" name="subcategory">
-        <option value="progulki-i-detskaya-komnata">
-          Детские товары и игрушки &gt; Прогулки и детская комната
-        </option>
-        <option value="kormlenie-i-gigiena">
-          Детские товары и игрушки &gt; Кормление и гигиена
-        </option>
+      <select class="form-control" name="subcategory" id="subcategory">
+        ${this.categories
+          .map((cat) => {
+            return this.getCategoryTemplate(cat);
+          })
+          .join("")}
       </select>
     </div>`;
+  }
+  getCategoryTemplate(cat) {
+    return cat.subcategories
+      .map((item) => {
+        return this.getSubcategoryTemplate(cat.title, item);
+      })
+      .join("");
+  }
+  getSubcategoryTemplate(parentName, item) {
+    return `<option value="${item.id}">${parentName} > ${item.title}</option>`;
   }
   getPropertiesTemplate() {
     return `<div class="form-group form-group__half_left form-group__two-col">
           <fieldset>
             <label class="form-label">Цена ($)</label>
             <input
+              value="${this.product ? this.product.price || "" : ""}"
               required=""
               type="number"
               name="price"
+              id="price"
               class="form-control"
               placeholder="100"
             />
@@ -127,9 +204,11 @@ export default class ProductForm {
           <fieldset>
             <label class="form-label">Скидка ($)</label>
             <input
+              value="${this.product ? this.product.discount || "" : ""}"
               required=""
               type="number"
               name="discount"
+              id="discount"
               class="form-control"
               placeholder="0"
             />
@@ -138,18 +217,24 @@ export default class ProductForm {
         <div class="form-group form-group__part-half">
           <label class="form-label">Количество</label>
           <input
+            value="${this.product ? this.product.quantity || "" : ""}"
             required=""
             type="number"
             class="form-control"
             name="quantity"
+            id="quantity"
             placeholder="1"
           />
         </div>
         <div class="form-group form-group__part-half">
           <label class="form-label">Статус</label>
-          <select class="form-control" name="status">
-            <option value="1">Активен</option>
-            <option value="0">Неактивен</option>
+          <select class="form-control" name="status" id="status" >
+            <option value="1" value="${
+              this.product && this.product.status === 1 ? "selected" : ""
+            }">Активен</option>
+            <option value="0" ${
+              this.product && this.product.status === 0 ? "selected" : ""
+            }>Неактивен</option>
           </select>
         </div>`;
   }
@@ -159,5 +244,16 @@ export default class ProductForm {
       Сохранить товар
     </button>
   </div>`;
+  }
+  //  /TEMPLATES
+
+  remove() {
+    // eslint-disable-next-line curly
+    if (this.element) this.element.remove();
+  }
+  destroy() {
+    this.remove();
+    this.element = null;
+    this.controller.abort();
   }
 }
