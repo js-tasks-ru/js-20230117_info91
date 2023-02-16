17a9781e44aba1c0759a04518d411a0cfbf04fdf
Дмитрий <info@consultapp.ru>
2023-02-17 01:54:02 +0400

    SortableList works + ProductForm fixes

--- 09-tests-for-frontend-apps/2-sortable-list/index.js
+++ 09-tests-for-frontend-apps/2-sortable-list/index.js
@@ -2,9 +2,9 @@ export default class SortableList {
   element = {};
   elementCoords = {};
   placeholder = {};
+  placeholderCoords = {};
   dragging = {};
   draggingShift = 0;
-  draggingHeight = 0;
 
   SHIFT_FROM_MOUSE = 5;
   controller = new AbortController();
@@ -28,7 +28,6 @@ export default class SortableList {
       item.classList.add("sortable-list__item");
       this.element.append(item);
     });
-    // this.element.append(this.placeholder);
 
     this.initListeners();
 
@@ -40,33 +39,73 @@ export default class SortableList {
       signal: this.controller.signal,
     });
   }
+
   pointerDownHandler = (event) => {
-    console.log(event.target);
-    this.dragging = event.target.closest(".sortable-list__item");
-    this.elementCoords = this.element.getBoundingClientRect();
-    this.draggingShift =
-      event.clientY - this.dragging.getBoundingClientRect().top;
+    const { target } = event;
 
-    document.addEventListener("pointerup", this.pointerUpHandler, {
-      signal: this.controller.signal,
-    });
-    document.addEventListener("pointermove", this.pointerMoveHandler, {
-      signal: this.controller.signal,
-    });
+    if (Object.hasOwn(target.dataset, "grabHandle")) {
+      this.dragging = target.closest(".sortable-list__item");
+      this.elementCoords = this.element.getBoundingClientRect();
+      this.draggingShift =
+        event.clientY - this.dragging.getBoundingClientRect().top;
 
-    this.startDragging(this.dragging);
-  };
+      document.addEventListener("pointerup", this.pointerUpHandler, {
+        signal: this.controller.signal,
+      });
+      document.addEventListener("pointermove", this.pointerMoveHandler, {
+        signal: this.controller.signal,
+      });
 
-  pointerMoveHandler = (event) => {
-    console.log(event.target);
-    if (
-      this.elementCoords.top < event.clientY - this.draggingShift &&
-      this.elementCoords.bottom >
-        event.clientY + this.draggingHeight - this.draggingShift
-    ) {
-      this.dragging.style.top = `${event.clientY - this.draggingShift}px`;
+      this.startDragging(this.dragging);
+    }
+
+    if (Object.hasOwn(target.dataset, "deleteHandle")) {
+      this.deleteItem(target.closest(".sortable-list__item"));
     }
   };
+
+  deleteItem(item) {
+    this.items = this.items.filter((element) => element !== item);
+    item.remove();
+  }
+
+  pointerMoveHandler = (event) => {
+    const top = event.clientY - this.draggingShift;
+
+    const isInParentRect =
+      this.elementCoords.top < top &&
+      this.elementCoords.bottom > top + this.placeholderCoords.height;
+
+    if (isInParentRect) {
+      this.dragging.style.top = `${top}px`;
+    }
+    if (event.clientY > this.elementCoords.bottom) {
+      this.dragging.style.top = `${
+        this.elementCoords.bottom - this.placeholderCoords.height
+      }px`;
+    }
+    if (event.clientY < this.elementCoords.top) {
+      this.dragging.style.top = `${this.elementCoords.top}px`;
+    }
+
+    this.testForSwitchingElements(top);
+  };
+
+  testForSwitchingElements(top) {
+    const isHigherOfPlaceholder =
+      top < this.placeholderCoords.top - this.placeholderCoords.height;
+    const isLowerOfPlaceholder = top > this.placeholderCoords.bottom;
+
+    if (isHigherOfPlaceholder && this.placeholder.previousSibling) {
+      this.placeholder.previousSibling.before(this.placeholder);
+      this.placeholderCoords = this.placeholder.getBoundingClientRect();
+    }
+
+    if (isLowerOfPlaceholder && this.placeholder.nextSibling) {
+      this.placeholder.nextSibling.after(this.placeholder);
+      this.placeholderCoords = this.placeholder.getBoundingClientRect();
+    }
+  }
 
   pointerUpHandler = () => {
     this.stopDragging(this.dragging);
@@ -86,14 +125,12 @@ export default class SortableList {
 
   startDragging(item) {
     item.before(this.placeholder);
-
-    const coords = this.placeholder.getBoundingClientRect();
+    this.placeholderCoords = this.placeholder.getBoundingClientRect();
 
     item.classList.add("sortable-list__item_dragging");
-    this.draggingHeight = coords.height;
-    item.style.width = `${coords.width}px`;
-    item.style.height = `${coords.height}px`;
-    item.style.top = `${coords.top + this.SHIFT_FROM_MOUSE}px`;
+    item.style.width = `${this.placeholderCoords.width}px`;
+    item.style.height = `${this.placeholderCoords.height}px`;
+    item.style.top = `${this.placeholderCoords.top + this.SHIFT_FROM_MOUSE}px`;
   }
 
   remove() {
@@ -104,6 +141,10 @@ export default class SortableList {
   destroy() {
     this.remove();
     this.element = null;
+    this.elementCoords = null;
+    this.placeholder = null;
+    this.placeholderCoords = null;
+    this.dragging = null;
     this.controller.abort();
   }
 }
